87b80ee054864bfab59f24a812eef02333e512b4
정수진 <soojinjung@jeongsujin-ui-MacBookPro.local>
2022-05-31 10:58:05 +0900

    수진- 없는숫자더하기

---
+++ 없는 숫자 더하기/soojin.js
@@ -0,0 +1,14 @@
+// 문제
+// 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. 
+// numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+
+let answer = 0;
+
+function solution(numbers) {
+    for (i=0; i< 10; i++) {
+        if (!numbers.includes(i)) {
+            answer += i;
+        }
+    }
+    return answer;
+}
