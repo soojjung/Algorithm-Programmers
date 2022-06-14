6a99ec6fe35f72f50b2b74cfa29f69708a238f51
박겸영 <yong_drg@naver.com>
2022-06-14 22:38:10 +0900

    Modify: 들여쓰기 수정

--- level 1/없는 숫자 더하기/kyeom.js
+++ level 1/없는 숫자 더하기/kyeom.js
@@ -1,16 +1,16 @@
 function solution1(numbers) {
-    let answer = 0;
-    for (let i = 0; i <= 9; i++) {
-        if (!numbers.includes(i)) {
-            answer += i;
-        }
-    }
-    return answer;
+  let answer = 0;
+  for (let i = 0; i <= 9; i++) {
+      if (!numbers.includes(i)) {
+          answer += i;
+      }
   }
-  
-  function solution2(numbers) {
-    return 45 - numbers.reduce((a, b) => a + b);
-  }
-  
-  solution1([1,2,3,4,6,7,8,0]); // 14
-  solution2([1,2,3,4,6,7,8,0]); // 14
+  return answer;
+}
+
+function solution2(numbers) {
+  return 45 - numbers.reduce((a, b) => a + b);
+}
+
+solution1([1,2,3,4,6,7,8,0]); // 14
+solution2([1,2,3,4,6,7,8,0]); // 14
